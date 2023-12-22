5416757dc996789b9a126526830ac5814048a88a
Leen73XX <leen.mej@gmail.com>
2023-12-22 22:08:09 +0300

    second

--- pillbell/ContentView.swift
+++ pillbell/ContentView.swift
@@ -11,60 +11,83 @@ import EventKit
 
 struct ContentView: View {
     
-
+    
     @Environment(\.modelContext) private var modelContext
     @Environment(\.colorScheme) var colorScheme
     @Query private var items: [Item]
-
+    @State private var showSettingsMenu = false
     var body: some View {
-        
+       
+            
+            //                .background(colorScheme == .dark ? Color.black : Color.black)
+           
         NavigationStack {
+            
             ScrollView{
-                Calender(interval: DateInterval(start:.distantPast, end: .distantPast),eventStore: EKEventStore())
-            }
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))")
+                
+                Button(action: {
+                    showSettingsMenu.toggle()
+                }) {
+                    Label("Add Item", systemImage: "plus").padding(.trailing, 250.0)}
+                
+                
+                
+                .navigationTitle("calender")
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarTrailing) {
+                        EditButton()
                     }
-                label: {
-                        Text(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))
-                    }
-                }
-                .onDelete(perform: deleteItems)
-            }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
+                    ToolbarItem {
+                        Button(action: addItem) {
+                            Label("Add Item", systemImage: "plus")
+                        }
                     }
                 }
+                
+                if showSettingsMenu {
+                    Calender(interval: DateInterval(start:.distantPast, end: .distantPast),eventStore: EKEventStore())
+                    
+                    List {
+                        ForEach(items) { item in
+                            NavigationLink {
+                                Text("Item at \(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))")
+                            }
+                        label: {
+                            Text(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))
+                        }
+                        }
+                        .onDelete(perform: deleteItems)
+                    }
+                    
+                }
+                
+                Text("hgfd")
+                           Text("lkjh")
             }
-            .navigationTitle("calender")
+          
         }
-    
-   
+                    
+            
         
     }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(timestamp: Date())
-            modelContext.insert(newItem)
+    private func cal(){
+       
+   }
+        private func addItem() {
+            withAnimation {
+                let newItem = Item(timestamp: Date())
+                modelContext.insert(newItem)
+            }
         }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            for index in offsets {
-                modelContext.delete(items[index])
+        
+        private func deleteItems(offsets: IndexSet) {
+            withAnimation {
+                for index in offsets {
+                    modelContext.delete(items[index])
+                }
             }
         }
     }
-}
 
 
 #Preview {
